9102d308fd54d20baf8da0eb18b45c48d8c9a0fb
yf-ashu <yfcai1227@gmail.com>
2018-04-24 22:45:40 +0800

    add no data alert

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,24 @@
+function deleteItem(e) {
+    let num = e.currentTarget.dataset.id;
+    alert('確定要刪除嗎？');
+    window.location.assign(`/${num}`);
+}
+function checkNull(e) {
+    e.preventDefault();
+    let input = document.querySelector('.input').value;
+    if (!input) {
+        alert('請輸入待辦事項！');
+    } else {
+        document.form.submit();
+    }
+}
 window.onload = function () {
     for (let [index, i] of item.entries()) {
         item[index].addEventListener('click', deleteItem, false);
         console.log(item[index]);
     }
+    btn.addEventListener('click', checkNull, false);
 }
 const item = document.querySelectorAll('.btn-delete');
-const btn = document.querySelectorAll('.btn');
+const btn = document.querySelector('.btn');
 
-function deleteItem(e) {
-    let num = e.currentTarget.dataset.id;
-    console.log(num);
-    alert('確定要刪除嗎？');
-    window.location.assign(`/${num}`);
-}
