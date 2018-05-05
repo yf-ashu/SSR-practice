75ff8abfe08195d8f813d52bc8a61e8a36ef0062
yf-ashu <yfcai1227@gmail.com>
2018-05-06 00:21:08 +0800

    bug

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 const action = document.querySelector('.action');
-const item = document.querySelectorAll('.item');
+const item = document.querySelectorAll('.item--inner');
 const deleteBtn = document.querySelectorAll('.btnDelete');
 const updateBtn = document.querySelectorAll('.btnUpdate');
 const signup = document.querySelector('.signup');
@@ -17,28 +17,42 @@ function btnDelete() {
       break;
   }
 }
+
 function cancel() {
   let set = this.dataset.num;
   item[set].disabled = true;
+  item[set].value = item[set].placeholder;
   updateBtn[set].textContent = '修改';
   deleteBtn[set].textContent = '刪除';
+  deleteBtn.forEach(input => (input.disabled = false));
+  updateBtn.forEach(input => (input.disabled = false));
+  console.log('3.' + updateBtn[set].textContent);
 }
 
 function btnUpdate() {
-  //可以重構成this[set]，呼叫updateItem(attr,attr2,'送出')
   let num = this.dataset.id ? this.dataset.id : '';
   let set = this.dataset.num;
+  console.log('1.' + updateBtn[set].textContent);
+
+  deleteBtn.forEach(input => (input.disabled = true));
+  updateBtn.forEach(input => (input.disabled = true));
   item[set].disabled = false;
-  updateBtn[set].textContent = '送出';
+  deleteBtn[set].disabled = false;
+  updateBtn[set].disabled = false;
   deleteBtn[set].textContent = '取消';
-  deleteBtn[set].addEventListener('click', cancel, false);
-  updateBtn[set].addEventListener(
-    'click',
-    function() {
-      document.getElementById('formUpdate').submit();
-    },
-    false
-  );
+
+  deleteBtn[set].addEventListener('click', cancel);
+  updateBtn[set].addEventListener('click', send);
+}
+
+function send() {
+  if (updateBtn[this.dataset.num].textContent === '送出') {
+    document.getElementById('formUpdate').submit();
+    return console.log('2.' + updateBtn[this.dataset.num].textContent);
+  } else {
+    updateBtn[this.dataset.num].textContent = '送出';
+    return console.log('4.' + updateBtn[this.dataset.num].textContent);
+  }
 }
 
 function checkNull(e) {
@@ -66,9 +80,9 @@ window.onload = function() {
     input.dataset.num = index;
   });
   updateBtn.forEach((input, index) => {
-    console.log(updateBtn[index].className);
-    input.addEventListener('click', btnUpdate, false);
+    // console.log(updateBtn[index].className);
     input.dataset.num = index;
+    return input.addEventListener('click', btnUpdate, false);
   });
 
   if (window.location == 'http://127.0.0.1:3000/task') {
